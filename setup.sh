2a438e7835415d0f589591ca1564f64db4516b8b
Mike Schmid <mikeschmid89@gmail.com>
2022-10-20 23:24:43 +0200

    symlink aliases etc.

--- setup.sh
+++ setup.sh
@@ -4,4 +4,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+
+# symlink aliases etc. and source .zshrc to apply changes
+ln -s $(pwd)/oh-my-zsh/custom_aliases.zsh ~/.oh-my-zsh/custom &&
+ln -s $(pwd)/oh-my-zsh/custom_env_variables.zsh ~/.oh-my-zsh/custom &&
+ln -s $(pwd)/oh-my-zsh/custom_functions.zsh ~/.oh-my-zsh/custom &&
+ln -s $(pwd)/oh-my-zsh/custom_startup.zsh ~/.oh-my-zsh/custom &&
+source ~/.zshrc
