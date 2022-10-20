4a0c6112196dee45121942b758b9f3c3e67ccc8c
Mike Schmid <mikeschmid89@gmail.com>
2022-10-20 23:45:47 +0200

    update setup.sh
    install stuff via homebrew

--- setup.sh
+++ setup.sh
@@ -6,9 +6,20 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
 
-# symlink aliases etc. and source .zshrc to apply changes
+# symlink aliases etc.
 ln -s $(pwd)/oh-my-zsh/custom_aliases.zsh ~/.oh-my-zsh/custom &&
 ln -s $(pwd)/oh-my-zsh/custom_env_variables.zsh ~/.oh-my-zsh/custom &&
 ln -s $(pwd)/oh-my-zsh/custom_functions.zsh ~/.oh-my-zsh/custom &&
 ln -s $(pwd)/oh-my-zsh/custom_startup.zsh ~/.oh-my-zsh/custom &&
+
+# install everythin listed in the Brewfile
+brew bundle &&
+
+# install fzf auto-completion
+/usr/local/opt/fzf/install &&
+
+# configure autojump
+sed -i '' 's/plugins=(git)/plugins=(git autojump)/g' ~/.zshrc &&
+
+# source .zshrc apply changes to active terminal
 source ~/.zshrc
