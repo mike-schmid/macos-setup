5ea1a4e7edaed0552b72e008a0beb299871d78e9
Mike Schmid <mikeschmid89@gmail.com>
2022-10-20 22:45:10 +0200

    add basic setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
