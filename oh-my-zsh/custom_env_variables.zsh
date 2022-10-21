9057406f3bf3b1056a3f04f4a6709736a26023a8
Mike Schmid <mikeschmid89@gmail.com>
2022-10-21 15:09:53 +0200

    use different bat theme

--- oh-my-zsh/custom_env_variables.zsh
+++ oh-my-zsh/custom_env_variables.zsh
@@ -2,3 +2,4 @@
 # Environment variables      |
 # ----------------------------
 export HISTSIZE=500000 # save that many entries in the command hsitory
+export BAT_THEME=Coldark-Dark # use a theme for bat
